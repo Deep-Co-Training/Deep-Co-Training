80617d0a0f05c914e50458e71b801202eba90e8f
Zayd-Jamadar77@gmai <zaydjamadar777@gmail.com>
2021-11-01 16:33:34 +0530

    Add Metric Logger

--- Deep-Co-Training/deep_co_training.py
+++ Deep-Co-Training/deep_co_training.py
@@ -2,6 +2,7 @@ import os
 import importlib
 import sys
 import time
+import datetime
 
 import tensorflow as tf
 from tensorflow import keras
@@ -20,6 +21,24 @@ optimizer = keras.optimizers.SGD(learning_rate=1e-3)
 loss_fn = keras.losses.MeanSquaredError()
 
 batch_size=2
+
+#Create stateful metrics that can be used to accumulate values during training and logged at any point
+train_loss_clf1 = tf.keras.losses.BinaryCrossentropy('train_loss', from_logits=True)
+train_accuracy_clf1 = keras.metrics.RootMeanSquaredError('train_accuracy')
+test_loss_clf1 = tf.keras.losses.BinaryCrossentropy('test_loss', from_logits=True)
+test_accuracy_clf1 = keras.metrics.RootMeanSquaredError('test_accuracy')
+
+#Set up summary writers to write the summaries to disk in a different logs directory
+current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+train_log_dir = 'logs/gradient_tape/' + current_time + '/train'
+test_log_dir = 'logs/gradient_tape/' + current_time + '/test'
+train_summary_writer = tf.summary.create_file_writer(train_log_dir)
+test_summary_writer = tf.summary.create_file_writer(test_log_dir)
+
+train_loss_clf2 = None
+train_accuracy_clf2 = None
+test_loss_clf2 = None
+test_accuracy_clf2 = None
 
 def import_model(subname):
 	mymodule=None
@@ -58,9 +77,19 @@ def custom_train(EPOCHS,c1,train_dataset):
 		print(train_dataset)
 		# Iterate over the batches of the dataset.
 		for step, (x_batch_train, y_batch_train) in enumerate(train_dataset):
+
 			print(step, x_batch_train, y_batch_train)
 			loss_value = train_step(x_batch_train, y_batch_train, c1)
-			
+
+			# Use tf.summary.scalar() to log metrics with the scope of the summary writers 
+			with train_summary_writer.as_default():
+				tf.summary.scalar('loss', train_loss_clf1.result(), step=epoch)
+				tf.summary.scalar('accuracy', train_accuracy_clf1.result(), step=epoch)
+
+			with train_summary_writer.as_default():
+				tf.summary.scalar('loss', train_loss_clf2.result(), step=epoch)
+				tf.summary.scalar('accuracy', train_accuracy_clf2.result(), step=epoch)
+
 			# Log every batch.
 			print(
 				"Training loss (for one batch) at step %d: %.4f"
@@ -79,10 +108,29 @@ def custom_train(EPOCHS,c1,train_dataset):
 		# for x_batch_val, y_batch_val in val_dataset:
 		# 	test_step(x_batch_val, y_batch_val, c1)
 
+		# with test_summary_writer.as_default():
+		# 	tf.summary.scalar('loss', test_loss_clf1.result(), step=epoch)
+		# 	tf.summary.scalar('accuracy', test_accuracy_clf1.result(), step=epoch)
+
+		# with test_summary_writer.as_default():
+		# 	tf.summary.scalar('loss', test_loss_clf2.result(), step=epoch)
+		# 	tf.summary.scalar('accuracy', test_accuracy_clf2.result(), step=epoch)
+
 		# val_acc = val_acc_metric.result()
 		# val_acc_metric.reset_states()
 		# print("Validation acc: %.4f" % (float(val_acc),))
 		# print("Time taken: %.2fs" % (time.time() - start_time))
+
+		train_loss_clf1.reset_states()
+		test_loss_clf1.reset_states()
+		train_accuracy_clf1.reset_states()
+		test_accuracy_clf1.reset_states()
+
+		train_loss_clf2.reset_states()
+		test_loss_clf2.reset_states()
+		train_accuracy_clf2.reset_states()
+		test_accuracy_clf2.reset_states()
+
 
 def deep_co_training():
 	'''Main method which executes the entire data processing
