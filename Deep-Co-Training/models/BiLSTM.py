cf0d205ae02b03efb1a9a7a9c32e891ba1518712
Zayd-Jamadar <zaydjamadar777gmail.com>
2021-11-13 00:04:28 +0530

    Add bert tokenizer to bilstm

--- Deep-Co-Training/models/BiLSTM.py
+++ Deep-Co-Training/models/BiLSTM.py
@@ -1,8 +1,16 @@
+import tensorflow as tf
+import tensorflow_hub as hub
 from keras.layers import Input, Bidirectional, TimeDistributed, Embedding, LSTM
 from keras.layers.core import Dense, Dropout, Activation, Flatten
 from keras.models import Sequential
 from keras.models import Model
 import numpy as np
+
+map_model_to_preprocess = {
+    "bert_en_uncased_L-12_H-768_A-12": "https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3"
+}
+bert_model_name = "bert_en_uncased_L-12_H-768_A-12"
+tfhub_handle_preprocess = map_model_to_preprocess[bert_model_name]
 
 
 vocab_size = 5000
@@ -15,14 +23,13 @@ class Lstm:
         pass
 
     def get_model(self):
-        text_input = Input(shape=(max_len,))
-        model = Embedding(
-            vocab_size, 1, weights=[embedding_matrix], input_length=max_len
-        )(text_input)
+        text_input = tf.keras.layers.Input(shape=(), dtype=tf.string, name="text")
+        preprocessing_layer = hub.KerasLayer(tfhub_handle_preprocess, name="preprocessing")
+        encoder_inputs = preprocessing_layer(text_input)
         model = Bidirectional(
             LSTM(1, return_sequences=True, dropout=0.50), merge_mode="concat"
-        )(model)
-        model = TimeDistributed(Dense(1, activation="relu"))(model)
+        )(encoder_inputs)
+        # model = TimeDistributed(Dense(1, activation="relu"))(model)
         model = Flatten()(model)
         model = Dense(128, activation="relu")(model)
         model = Dense(16, activation="relu")(model)
